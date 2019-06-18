ecef01e785ef725600f268f7dbf41d4d29248158
Angela Lo <angelawenlo@gmail.com>
2019-06-18 13:21:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,13 @@ require 'pry'
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
+  @@genres = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres = []
+
     binding.pry
   end
   def self.count
